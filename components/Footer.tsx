b3827a61cad7a66c6e0557aae344cd9ab9973470
KiranNepali <itsmekiran44@gmail.com>
2024-07-08 15:51:32 +0545

    changes

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -11,7 +11,7 @@ function Footer({}: Props) {
   const currentRoute = usePathname();
   return (
     <>
-      <footer className="bg-white shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),0px_1px_0px_0px_rgba(25,28,33,0.02),0px_0px_0px_1px_rgba(25,28,33,0.08)] lg:grid lg:grid-cols-5">
+      <footer className="bg-secondary-50 shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),0px_1px_0px_0px_rgba(25,28,33,0.02),0px_0px_0px_1px_rgba(25,28,33,0.08)] lg:grid lg:grid-cols-5">
         <div className="relative block h-32 lg:col-span-2 lg:h-full">
           <Image
             width={1000}
